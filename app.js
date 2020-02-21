509901debd5970cfc62457f5c440c2e5975cfb16
Gabriel La Tore <42503800+GabrielLaTorre@users.noreply.github.com>
2020-02-21 17:26:10 -0300

    upp

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
  var apiKey = "lO1NJjmLEEOMMlbZyPyx6EA0N4vEowCw";
-
+ var cards = [];
+ var uno = document.getElementsByTagName('#rec-1');
+ cards.push(uno);
+ console.log(cards);
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    
-    document.getElementById("boton").addEventListener("click", ev => {
+
+  // ############ ENDPOINT DE RANDOM ##############
+  /*fetch('https://api.giphy.com/v1/gifs/random?api_key=lO1NJjmLEEOMMlbZyPyx6EA0N4vEowCw&tag=&rating=G')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.log(err))
+
+  
+
+  // ############ ENDPOINT DE SEARCH ##############
+
+  /*  document.getElementById("boton").addEventListener("click", ev => {
     ev.preventDefault();
     var search = document.getElementById('input').value;
     fetch(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}`)
@@ -12,12 +27,12 @@ function init() {
       .then(content => {
         var src = content.data[0].images.downsized.url;
         let img = document.createElement('img');
-        let contenedor = document.getElementById('contenido');
+        let contenedor = document.getElementById('tendencias');
         img.setAttribute('src', src);
         contenedor.insertAdjacentElement('afterbegin', img);
       })
       .catch(error => {
         console.log(error);
       });
-  });
+  }); */
 }
