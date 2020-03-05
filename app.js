88bd165f78c55013e707d9121f10c6bc2b6e5e3e
Gabriel La Tore <42503800+GabrielLaTorre@users.noreply.github.com>
2020-03-05 00:20:51 -0300

    up

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ function switchTeme(e) {
   let light = 'styles.css'
   var theme = document.getElementById('style');
   if (e.target.id == 'day') {
+    var logo = document.getElementById('logo');
+    logo.setAttribute('src', '/images/logo.png')
     theme.setAttribute('href', light);
   } 
   else if (e.target.id == 'night') {;
+    var logo = document.getElementById('logo');
+    logo.setAttribute('src', '/images/logo_dark.png')
     theme.setAttribute('href', dark);
   }  
 }
